8e615a7b2a8d85a1e2ee51dcab0144bd139d5527
Kanishk Sharma-MBP <kanishksharma-mbp@tokopedias-MacBook-Pro-3.local>
2019-09-12 12:36:37 +0530

    location props missing add

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,24 @@
-declare module 'react-native-shimmer-placeholder' {
-    import React from 'react';
-    import { Animated } from 'react-native';
-    interface Props {
-        width?: number | string;
-        height?: number | string;
-        widthShimmer?: number;
-        duration?: number;
-        delay?: number;
-        colorShimmer?: string[];
-        reverse?: boolean;
-        autoRun?: boolean;
-        visible?: boolean;
-        children?: any;
-        style?: any;
-        backgroundColorBehindBorder?: string;
-        hasBorder?: boolean;
-        isInteraction?: boolean;
-    }
-    export default class ShimmerPlaceHolder extends React.Component<Props, any> {
-        getAnimated(): Animated.CompositeAnimation;
-    }
+declare module "react-native-shimmer-placeholder" {
+  import React from "react";
+  import { Animated } from "react-native";
+  interface Props {
+    width?: number | string;
+    height?: number | string;
+    location?: number[];
+    widthShimmer?: number;
+    duration?: number;
+    delay?: number;
+    colorShimmer?: string[];
+    reverse?: boolean;
+    autoRun?: boolean;
+    visible?: boolean;
+    children?: any;
+    style?: any;
+    backgroundColorBehindBorder?: string;
+    hasBorder?: boolean;
+    isInteraction?: boolean;
+  }
+  export default class ShimmerPlaceHolder extends React.Component<Props, any> {
+    getAnimated(): Animated.CompositeAnimation;
+  }
 }
